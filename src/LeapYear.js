bb7ef424b23a538d4ac5255661c61cab242c8726
shafali03 <shaf03sm@gmail.com>
2020-02-24 12:53:08 +0000

    test for leap year 2000 passing and refactor code

--- src/LeapYear.js
+++ src/LeapYear.js
@@ -2,9 +2,5 @@
 function Leap() {}
 
 Leap.prototype.leapYears = function(year) {
-  if ((year % 4 == 0) && (year % 100 != 0)) {
-    return true
-  } else {
-    return false
-  }
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) 
 }
